26b384823be6dfdfbefc43c3bd865321ced85130
cecildt <cecildt@gmail.com>
2023-01-25 22:46:51 +0100

    Add DaisyUI themes switcher

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
+import { useEffect } from "react";
 import "./App.css";
+import { ThemeSwitcher } from "./components/theme-switcher";
+import { DashboardStats } from "./components/dashboard-stats";
 
 function App() {
   return (
     <div className="App">
+      <ThemeSwitcher />
+      <DashboardStats />
       <h1 className="text-3xl font-bold underline">Hello Secure Desk!</h1>
+      <button className="btn btn-primary">Button</button>
     </div>
   );
 }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,tsx,ts}"],
+  content: ["./index.html", "./src/**/*.{html,js,tsx,ts}"],
   theme: {
     extend: {},
   },
-  plugins: [require("daisyui")],
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
 };
